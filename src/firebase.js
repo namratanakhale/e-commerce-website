6a95318824b0b4138895bd0e175a1eb77159c2d1
namratanakhale <namratanakhale91@gmail.com>
2022-12-12 20:42:10 +0530

    configuration added in firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
-  authDomain: "challenge-4b2b2.firebaseapp.com",
-  databaseURL: "https://challenge-4b2b2.firebaseio.com",
-  projectId: "challenge-4b2b2",
-  storageBucket: "challenge-4b2b2.appspot.com",
-  messagingSenderId: "962418448875",
-  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",
+  apiKey: "AIzaSyBty3tAt6A6LnKkhzestN9VAkFdqiLnfPY",
+  authDomain: "e-commerce-web-clone.firebaseapp.com",
+  projectId: "e-commerce-web-clone",
+  storageBucket: "e-commerce-web-clone.appspot.com",
+  messagingSenderId: "741033072661",
+  appId: "1:741033072661:web:57b3a2ffec8f83b0ff561c",
+  measurementId: "G-3BV3EESHZ3"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
