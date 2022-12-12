638cb9223e2b99de0f103e11a4c8e5e869bcee8d
namratanakhale <namratanakhale91@gmail.com>
2022-12-12 20:23:27 +0530

    dependencies added

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,40 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
-);
+const stripe = require("stripe")('sk_test_51MC4XySDkAcNApaVatXLXVg3F4cmgrmZjVEGpVKdl2UMXq3mEzb3JO8ffgsZJUd1VIrsDmDEVk2GuSuT9bRrO6NJ00GykRYveo')//Secret Key
 
-// API
+//API
 
-// - App config
+//- API config
 const app = express();
 
-// - Middlewares
-app.use(cors({ origin: true }));
+//- Middlewares
+app.use(cors({origin: true}));
 app.use(express.json());
 
-// - API routes
-app.get("/", (request, response) => response.status(200).send("hello world"));
+//- API routes
+app.get('/', (request, response)=> response.status(200).send('hello world'));
 
-app.post("/payments/create", async (request, response) => {
-  const total = request.query.total;
 
-  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+app.post("/payments/create", async (request,response)=>{
+    const total = request.query.total;
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total, // subunits of the currency
-    currency: "usd",
-  });
+    console.log("Payment Request Received BOOM!! for this amount >>>", total);
 
-  // OK - Created
-  response.status(201).send({
-    clientSecret: paymentIntent.client_secret,
-  });
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, //subunits of the currency
+        currency: "usd",
+    });
+
+    //OK - Created
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
 });
 
-// - Listen command
+//- Listen command
 exports.api = functions.https.onRequest(app);
 
-// Example endpoint
-// http://localhost:5001/challenge-4b2b2/us-central1/api
+//Example endpoint 
+//http function initialized (http://127.0.0.1:5001/clone-2c7c1/us-central1/api)//prints 'hello world'.
+
