86234cfde2b968914b1a5e5dd79c0f50051cee0c
namratanakhale <namratanakhale91@gmail.com>
2022-12-21 18:01:23 +0530

    rating change

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -58,7 +58,7 @@ function Home() {
             id="90829332"
             title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
             price={1094.98}
-            rating={4}
+            rating={5}
             image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
           />
         </div>
